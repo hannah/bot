64b59dd0461bb23796c6028c07d42f46c416989c
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-04-13 14:30:26 -0400

    Added urban dictionary search function

--- bot.rb
+++ bot.rb
@@ -27,6 +27,25 @@ class Google
   end
 end
 
+class UrbanDictionary
+  include Cinch::Plugin
+  match /ud (.+)/
+
+  def search(query)
+    url = "http://www.urbandictionary.com/define.php?term=#{CGI.escape(query)}"
+    result = Nokogiri::HTML(open(url)).at(".meaning")
+    meaning = result.text
+
+    CGI.unescape_html "#{meaning}"
+  rescue
+    "No results found"
+  end
+
+  def execute(m, query)
+    m.reply(search(query))
+  end
+end
+
 
 class DoMath
   include Cinch::Plugin
@@ -60,8 +79,8 @@ bot = Cinch::Bot.new do
   configure do |c|
     c.nick = "nothubot"
     c.server = "chat.freenode.net"
-    c.channels = ["#femalefashionadvice"]
-    c.plugins.plugins = [Google, DoMath]
+    c.channels = ["#cinch-bots"]
+    c.plugins.plugins = [Google, DoMath, UrbanDictionary]
   end
 
 
