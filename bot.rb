a5bb3791485b0ea88e2ab41cddd09ca7e2dd9b42
Billy Fung <billy@billyfung.com>
2014-04-29 11:57:21 -0700

    added wolframalpha

--- bot.rb
+++ bot.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'cgi'
 
+
 ### Substitute values for YOUR_BOT_NAME and YOUR_SERVER_NAME
 class Google
   include Cinch::Plugin
@@ -60,6 +61,36 @@ class DoMath
     m.reply(calc(query))
   end
 end
+ 
+class Wolframsearch
+  include Cinch::Plugin
+  #plugin "wolfram"
+ 
+  match /wa (.+)/
+ 
+  def self.search(query)
+    url = "http://www.wolframalpha.com/input/?i=#{CGI.escape(query)}"
+    
+    # Get API key from
+    #          https://developer.wolframalpha.com/portal/signin.html
+    options = { "format" => "plaintext" } # see the reference appendix in the documentation.[1]w
+    client = WolframAlpha::Client.new "855858-G8QRY7HHWQ", options
+ 
+    query = client.query(query)
+    #if query.result
+      result = query.find { |pod| pod.title == "Result" } # Get the result pod
+      result = result.subpods[0].plaintext
+    #else
+     #"Sorry, I've no idea"
+    #end
+  rescue
+    url
+  end
+ 
+  def execute(m, query)
+    m.reply self.class.search(query), true #self.class.search
+  end
+end
 
 # class Bookmark
 #   include Cinch::Plugin
@@ -73,10 +104,10 @@ end
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.nick = "neonbot"
+    c.nick = "neonbot2"
     c.server = "dickson.freenode.net"
-    c.channels = ["#cinch-bots"]
-    c.plugins.plugins = [Google, DoMath, UrbanDictionary]
+    c.channels = ["#femalefashionadvice"]
+    c.plugins.plugins = [Google, DoMath, UrbanDictionary, Wolframsearch]
   end
 
   on :message, "hi" do |m|
